52c81677f5d42bf1b8ca3a219dcaff1faa951237
GENZxM <97391354+GENZxM@users.noreply.github.com>
2022-02-21 23:57:13 +0800

    Create translation.py

---
+++ translation.py
@@ -0,0 +1,8 @@
+"""
+This Bot Was Developed By The Owner Of @XFlick.
+Join his network and support him.
+"""
+class Translation:
+  START_TEXT = "✫Hᴇʟʟᴏ, I ᴀᴍ Z-AnimexBot. I ᴄᴀɴ help you to search anime information.\nKɪɴᴅʟʏ, Jᴏɪɴ ᴍʏ ɴᴇᴛᴡᴏʀᴋ Too✫"
+  
+  HELP_TEXT = "✫I think you don't need a help✫"
